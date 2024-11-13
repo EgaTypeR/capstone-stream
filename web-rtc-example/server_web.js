1c6ac378f6e05afadb519e442cf2045905ff1694
EgaTypeR <ega.rizky.setiawan@mail.ugm.ac.id>
2024-11-14 03:12:34 +0700

    fix path

--- web-rtc-example/server_web.js
+++ web-rtc-example/server_web.js
@@ -10,12 +10,12 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 // Serve static files
-const path = __dirname + '/'
-app.use(express.static(path));
+const dir_path = __dirname + '/'
+app.use(express.static(dir_path));
 
 // Start the server
 const PORT = process.env.PORT || 3030;
 server.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
-    console.log(path)
+    console.log(dir_path)
 });
